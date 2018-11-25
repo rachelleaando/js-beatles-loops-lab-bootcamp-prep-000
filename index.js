f694869fdd7ce4ca7ccbf4bb9a027f360119e784
Rachelle Ando <rachellelorraineandoart@gmail.com>
2018-11-25 16:12:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,16 +5,10 @@
    }
    return empty;
  }
-function johnLennonFacts(array){
-  var i = 0;
- var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+function johnLennonFacts(array) {
+  var i = 0
   while (i < array.length) {
-    array.push(facts + "!!!");
+    array[i] += '!!!';
     i++;
   }
   return array;
